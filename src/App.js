4d6f82824b63bd48b7dffcbb6736d12cd8e8f2d7
edwin6666 <edwinmendoza0620@gmail.com>
2023-02-28 13:57:14 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+import ClickLogo from './imagenes/images.png';
+
+function App() {
+  return (
+    
+    <div className="App">
+      
+      <div class='contenedor-logo'>
+        <img className='click-logo'
+        src={ClickLogo} 
+        alt='click logo'
+        />
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
